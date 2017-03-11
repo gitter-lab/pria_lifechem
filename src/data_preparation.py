b5163fe8e994eaaa68bbc76926ebc1f69698edee
lscHacker <lschacker@hotmail.com>
2017-03-11 09:05:17 -0600

    Update function

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -10,33 +10,35 @@ def transform_data(input_file_name, output_file_name, updated_binary_label):
     good = 0
     bad = 0
     i = 0
+    unique = set()
     print >> output_file, "molecule ID(RegID),library,existing SMILES,generated SMILES,1024_fingerprint,Pria_SSB_%INH,true_label"
     for mol in suppl:
         i += 1
         if mol is None:
             bad += 1
             continue
-        good += 1
         name = mol.GetProp("RegID")
+        if name in unique:
+            continue
+        unique.add(name)
+        good += 1
         library = mol.GetProp("Library")
         existing_smiles = mol.GetProp("SMILES")
         generated_smiles = Chem.CanonSmiles(Chem.MolToSmiles(mol))
         generated_mol = Chem.MolFromSmiles(existing_smiles)
         fingerprints = AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=1024)
-        
+
         pria = mol.GetProp("Pria_SSB_%INH")
         pria = float(pria)
-        
+
         true_label = updated_binary_label[updated_binary_label['Molecule'] == name]['Keck_Pria_AS_Retest']
         true_label = true_label.tolist()[0]
-#         print name, continuous_data[continuous_data['Molecule'] == name]['Keck_Pria_Continuous']
-#         print name, updated_binary_label[updated_binary_label['Molecule'] == name]['Keck_Pria_AS_Retest']
-        output_line = "{},{},{},{},{},{},{}".format(name, 
-                                                    library, 
-                                                    existing_smiles, 
-                                                    generated_smiles, 
-                                                    fingerprints.ToBitString(), 
-                                                    pria, 
+        output_line = "{},{},{},{},{},{},{}".format(name,
+                                                    library,
+                                                    existing_smiles,
+                                                    generated_smiles,
+                                                    fingerprints.ToBitString(),
+                                                    pria,
                                                     true_label)
         print >> output_file, output_line
     print("{} items in all, {} are good, {} are bad".format(i, good, bad))
