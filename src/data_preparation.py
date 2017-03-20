724ed756903e6973b97cc02e3bfb98ce35d02eb8
Shengchao Liu <lscHacker@users.noreply.github.com>
2017-03-19 23:41:53 -0500

    Chao (#8)
    * Move demo.py into src
    
    * Remove debugging info in function.py
    
    * Rename it in evaluation.py
    
    * update function.py and readme
    
    * add raw data
    
    * add data preparation function
    
    * remove data due to file size
    
    * add demo
    
    * Update README on data and demo
    
    * Chao (#5)
    
    * Move demo.py into src
    
    * Remove debugging info in function.py
    
    * Rename it in evaluation.py
    
    * update function.py and readme
    
    * add raw data
    
    * add data preparation function
    
    * remove data due to file size
    
    * add demo
    
    * Update README on data and demo
    
    * Update function
    
    * update demo
    
    * update
    
    * update'
    
    * update data preparation
    
    * update data preparation, splitting, and merging
    
    * update functions for multi-task feature and label extraction
    
    * update demo
    
    * update demo

---
+++ src/data_preparation.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+from rdkit.Chem import AllChem
+from rdkit import Chem
+import operator
+
+
+
+def transform_data(output_file_name):
+    discrete_file = pd.ExcelFile('../dataset/data_preprocessing/screening_smsf_actives_2017_03_10.xlsx')
+    Keck_Pria_Retest = discrete_file.parse('Keck_Pria_Retest')
+    Keck_Pria_FP = discrete_file.parse('Keck_Pria_FP')
+    Keck_RMI = discrete_file.parse('Keck_RMI')
+    Xing_MTDH_Retest = discrete_file.parse('Xing_MTDH_Retest')
+    Xing_MTDH_DR = discrete_file.parse('Xing_MTDH_DR')
+    
+    continuous_file = pd.ExcelFile('../dataset/data_preprocessing/screening_smsf_continuous_2017_03_10.xlsx')
+    Keck_Pria_Primary = continuous_file.parse('Keck_Pria_Primary')
+    Keck_RMI_cdd = continuous_file.parse('Keck_RMI_cdd')
+    Xing_MTDH_cdd = continuous_file.parse('Xing_MTDH_cdd')
+    
+    f = open('../dataset/data_preprocessing/lifechem123_cleaned_2017_03_10.smi', 'r')
+    mol_smile_dict = {}
+    mol_fps_dict = {}
+
+    for line in f:
+        line = line.strip()
+        row = line.split(' ')
+        smiles = row[0]
+        molecule_id = row[1]
+        
+        # Get SMILE
+        mol_smile_dict[molecule_id] = smiles
+        
+        # Get molecule descriptor from SMILES
+        # Then generate Fingerprints from molecule descriptor
+        mol = Chem.MolFromSmiles(smiles)
+        fingerprints = AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=1024)
+        mol_fps_dict[molecule_id] = fingerprints.ToBitString()
+        
+    mol_smile_map = sorted(mol_smile_dict.items())
+    molecules = [item[0] for item in mol_smile_map]
+    smiles = [item[1] for item in mol_smile_map]
+    smiles_df = pd.DataFrame({'Molecule': molecules, 'SMILES': smiles})
+    
+    molecules = [key for key,_ in mol_fps_dict.iteritems()]
+    fingerprints = [value for _,value in mol_fps_dict.iteritems()]
+    fingerprints_df = pd.DataFrame({'Molecule': molecules, 'Fingerprints': fingerprints})
+    
+    result = pd.merge(smiles_df, fingerprints_df, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_Pria_Retest, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_Pria_FP, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_Pria_Primary, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_RMI, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_RMI_cdd, on='Molecule', how='outer')
+
+    result.to_csv(output_file_name, index=None)
+
+    return
+
+
+if __name__ == '__main__':
+    transform_data('../dataset/keck_complete.csv')
--- src/function.py
+++ src/function.py
@@ -1,97 +1,190 @@
 import pandas as pd
-import csv
-import json
 import numpy as np
-from sklearn.metrics import roc_auc_score, average_precision_score
+import random
+import os
+from sklearn.cross_validation import StratifiedKFold, KFold
 
 
-def roc_auc_multi(y_true, y_pred, eval_indices, eval_mean_or_median):
-    '''
-    this if for multi-task evaluation
-    y_true and y_pred is two-dimension matrix
-    can evaluate on mean or median of array
-    call by
-    roc_auc_multi(y_true, y_pred, [-1], np.mean)
-    roc_auc_multi(y_true, y_pred, [0], np.median)
-    '''
-    y_true = y_true[:, eval_indices]
-    y_pred = y_pred[:, eval_indices]
-    nb_classes = y_true.shape[1]
-    auc = np.zeros(nb_classes)
-    for i in range(len(auc)):
-        # -1 represents missing value
-        # and remove them when in evaluation
-        non_missing_indices = np.argwhere(y_true[:, i] != -1)[:, 0]
-        actual = y_true[non_missing_indices, i]
-        predicted = y_pred[non_missing_indices, i]
-        auc[i] = roc_auc_single(actual, predicted)
-    return eval_mean_or_median(auc)
+'''
+Analyze the original dataset
+'''
+def analysis(data):
+    class Node:
+        def __init__(self, retest, fp, rmi):
+            self.retest = retest
+            self.fp = fp
+            self.rmi = rmi
+            if not np.isnan(self.rmi):
+                self.rmi = int(self.rmi)
+            else:
+                self.rmi = np.NaN
+            
+        def __str__(self):
+            ret = 'retest: {}, fp: {}, rmi: {}'.format(self.retest, self.fp, self.rmi)
+            return ret
+        
+        def __eq__(self, other):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+        
+        def __hash__(self):
+            return hash(self.retest) ^ hash(self.fp) ^ hash(self.rmi)
+        
+        def __cmp__(self):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+    
+    dict_ = {}
+    for ix, row in data.iterrows():
+        node = Node(row['Keck_Pria_AS_Retest'], row['Keck_Pria_FP_data'], row['Keck_RMI_cdd'])
+        if node not in dict_.keys():
+            dict_[node] = 1
+        else:
+            dict_[node] += 1
+    
+    for k in dict_.keys():
+        print k, '\t---', dict_[k]
+    
+    return
 
 
-def roc_auc_single(actual, predicted):
-    return roc_auc_score(actual, predicted)
+'''
+Apply greedy method to split data when merging multi-task
+'''
+def greedy_multi_splitting(data, k, directory, file_list):
+    class Node:
+        def __init__(self, retest, fp, rmi):
+            self.retest = retest
+            self.fp = fp
+            self.rmi = rmi
+            if not np.isnan(self.rmi):
+                self.rmi = int(self.rmi)
+            else:
+                self.rmi = np.NaN
+            
+        def __str__(self):
+            ret = 'retest: {}, fp: {}, rmi: {}'.format(self.retest, self.fp, self.rmi)
+            return ret
+        
+        def __eq__(self, other):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+        
+        def __hash__(self):
+            return hash(self.retest) ^ hash(self.fp) ^ hash(self.rmi)
+        
+        def __cmp__(self):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+    
+    dict_ = {}
+    for ix, row in data.iterrows():
+        node = Node(row['Keck_Pria_AS_Retest'], row['Keck_Pria_FP_data'], row['Keck_RMI_cdd'])
+        if node not in dict_.keys():
+            dict_[node] = []
+        dict_[node].append(ix)
+        
+    list_ = []
+    for key in dict_.keys():
+        one_group_list = np.array(dict_[key])
+        current = []
+
+        if len(one_group_list) < k:
+            n = len(one_group_list)
+            for i in range(n):
+                current.append(np.array(one_group_list[i]))
+            for i in range(n, k):
+                current.append(np.array([]))
+        else:
+            kf = KFold(len(one_group_list), k, shuffle=True)
+            for _, test_index in kf:
+                current.append(one_group_list[test_index])
+        random.shuffle(current)
+        list_.append(current)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        
+    print len(list_)
+
+    for split in range(k):
+        index_block = np.hstack((list_[0][split],
+                                 list_[1][split],
+                                 list_[2][split],
+                                 list_[3][split],
+                                 list_[4][split],
+                                 list_[5][split],
+                                 list_[6][split],
+                                 list_[7][split],
+                                 list_[8][split]))
+        index_block = index_block.astype(np.int)
+        df_block = data.iloc[index_block]
+        print df_block.shape
+
+        file_path = directory + file_list[split]
+        df_block.to_csv(file_path, index=None)
+    
+    return
 
 
-def precision_auc_multi(y_true, y_pred, eval_indices, eval_mean_or_median):
-    '''
-    this if for multi-task evaluation
-    y_true and y_pred is two-dimension matrix
-    can evaluate on mean or median of array
-    call by
-    precision_auc_multi(y_true, y_pred, [-1], np.mean)
-    precision_auc_multi(y_true, y_pred, [0], np.median)
-    '''
-    y_true = y_true[:, eval_indices]
-    y_pred = y_pred[:, eval_indices]
-    nb_classes = y_true.shape[1]
-    auc = np.zeros(nb_classes)
-    for i in range(len(auc)):
-        # -1 represents missing value
-        # and remove them when in evaluation
-        non_missing_indices = np.argwhere(y_true[:, i] != -1)[:, 0]
-        actual = y_true[non_missing_indices, i]
-        predicted = y_pred[non_missing_indices, i]
-        auc[i] = precision_auc_single(actual, predicted)
-    return eval_mean_or_median(auc)
+
+'''
+input_file is whole 
+output_file_list is the files that we will split data into
+Make sure the output_file_list length is equal to k
+'''
+def split_data(input_file, output_file_list, k):
+    data_pd = pd.read_csv(input_file)
+    y_data = data_pd['true_label']
+    y_data = y_data.astype(np.float64)
+    if y_data.ndim == 1:
+        n = y_data.shape[0]
+        y_data = y_data.reshape(n, 1)
+
+    cnt = 0
+    split = StratifiedKFold(y_data[:, -1], n_folds=k, shuffle=True, random_state=0)
+    for train_index, test_index in split:
+        # For testing
+        # Can list all existing active ones
+        # data_batch[data_batch['true_label']>0]['molecule ID(RegID)']
+        data_batch = data_pd.iloc[test_index]
+        data_batch.to_csv(output_file_list[cnt], index_label=None, compression='gzip')
+        cnt += 1
+    return
 
 
-def precision_auc_single(actual, predicted):
-    return average_precision_score(actual, predicted)
+'''
+Read the data from all files in input_file_list
+And merged into one dataset
+'''
+def read_merged_data(input_file_list):
+    whole_pd = pd.DataFrame()
+    for input_file in input_file_list:
+        data_pd = pd.read_csv(input_file)
+        whole_pd = whole_pd.append(data_pd)
+    return whole_pd
 
 
-def enrichment_factor_multi(actual, predicted, percentile):
-    EF_list = []
-    for i in range(actual.shape[1]):
-        n_actives, ef = enrichment_factor_single(actual[:, i], predicted[:, i], percentile)
-        temp = [n_actives, ef]
-        EF_list.append(temp)
-    return EF_list
+'''
+Get the fingerprints, with feature_name specified, and label_name specified
+'''
+def extract_feature_and_label(data_pd,
+                              feature_name,
+                              label_name_list):
+    X_data = np.zeros(shape=(data_pd.shape[0], 1024))
+    y_data = np.zeros(shape=(data_pd.shape[0], len(label_name_list)))
+    for index, row in data_pd.iterrows():
+        feature = list(row[feature_name])
+        labels = row[label_name_list]
+        X_data[index] = np.array(feature)
+        y_data[index] = np.array(labels)
+    X_data = X_data.astype(float)
+    y_data = y_data.astype(float)
 
+    # In case we just train on one target
+    # y would be (n,) vector
+    # then we should change it to (n,1) 1D matrix
+    # to keep consistency
+    print y_data.shape
+    if y_data.ndim == 1:
+        n = y_data.shape[0]
+        y_data = y_data.reshape(n, 1)
 
-def enrichment_factor_single(labels_arr, scores_arr, percentile):
-    '''
-    calculate the enrichment factor based on some upper fraction
-    of library ordered by docking scores. upper fraction is determined
-    by percentile (actually a fraction of value 0.0-1.0)
+    return X_data, y_data
 
-    -1 represents missing value
-    and remove them when in evaluation
-    '''
-    non_missing_indices = np.argwhere(labels_arr!=-1)[:, 0]
-    labels_arr = labels_arr[non_missing_indices]
-    scores_arr = scores_arr[non_missing_indices]
-
-    sample_size = int(labels_arr.shape[0] * percentile)         # determine number mols in subset
-    pred = np.sort(scores_arr)[::-1][:sample_size]              # sort the scores list, take top subset from library
-    indices = np.argsort(scores_arr)[::-1][:sample_size]        # get the index positions for these in library
-    n_actives = np.nansum(labels_arr)                           # count number of positive labels in library
-    n_experimental = np.nansum(labels_arr[indices])            # count number of positive labels in subset
-    print 'sample size: {} '.format(sample_size)
-    print("active: {},\thit: {} ".format(n_actives, n_experimental))
-    temp = scores_arr[indices]
-    print('larget prob: {}'.format(temp[0]))
-    if n_actives > 0.0:
-        ef = float(n_experimental) / n_actives / percentile     # calc EF at percentile
-    else:
-        ef = 'ND'
-    return n_actives, ef
