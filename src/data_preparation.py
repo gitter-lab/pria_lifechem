44736bcdb61bec9436c291d43b0267a36b42a6ca
lscHacker <lschacker@hotmail.com>
2017-03-19 23:21:31 -0500

    update data preparation, splitting, and merging

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -5,37 +5,8 @@ from rdkit import Chem
 import operator
 
 
-def transform_data(input_file_name, output_file_name):
-    from collections import OrderedDict
 
-    f = open('../dataset/data_preprocessing/lifechem123_cleaned_2017_03_10.smi', 'r')
-    mol_smile_dict = {}
-
-    for line in f:
-        line = line.strip()
-        row = line.split(' ')
-        smiles = row[0]
-        mol = row[1]
-        mol_smile_dict[mol] = smiles
-
-    mol_smile_map = sorted(mol_smile_dict.items())
-    
-    molecules = [item[0] for item in mol_smile_map]
-    smiles = [item[1] for item in mol_smile_map]
-    
-    df = pd.DataFrame({'Molecule': molecules, 'SMILES': smiles})
-    result = pd.merge(df, Keck_Pria_Retest, on='Molecule', how='outer')
-    result = pd.merge(result, Keck_Pria_FP, on='Molecule', how='outer')
-    result = pd.merge(result, Keck_Pria_Primary, on='Molecule', how='outer')
-    result = pd.merge(result, Keck_RMI, on='Molecule', how='outer')
-    result = pd.merge(result, Keck_RMI_cdd, on='Molecule', how='outer')
-
-    result.to_csv(output_file_name, index=None)
-
-    return
-
-
-if __name__ == '__main__':
+def transform_data(output_file_name):
     discrete_file = pd.ExcelFile('../dataset/data_preprocessing/screening_smsf_actives_2017_03_10.xlsx')
     Keck_Pria_Retest = discrete_file.parse('Keck_Pria_Retest')
     Keck_Pria_FP = discrete_file.parse('Keck_Pria_FP')
@@ -47,5 +18,46 @@ if __name__ == '__main__':
     Keck_Pria_Primary = continuous_file.parse('Keck_Pria_Primary')
     Keck_RMI_cdd = continuous_file.parse('Keck_RMI_cdd')
     Xing_MTDH_cdd = continuous_file.parse('Xing_MTDH_cdd')
+    
+    f = open('../dataset/data_preprocessing/lifechem123_cleaned_2017_03_10.smi', 'r')
+    mol_smile_dict = {}
+    mol_fps_dict = {}
+
+    for line in f:
+        line = line.strip()
+        row = line.split(' ')
+        smiles = row[0]
+        molecule_id = row[1]
         
-    transform_data('../dataset/data_preprocessing/lifechem123_cleaned_2017_03_10.smi', '../dataset/keck_complete.csv')
+        # Get SMILE
+        mol_smile_dict[molecule_id] = smiles
+        
+        # Get molecule descriptor from SMILES
+        # Then generate Fingerprints from molecule descriptor
+        mol = Chem.MolFromSmiles(smiles)
+        fingerprints = AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=1024)
+        mol_fps_dict[molecule_id] = fingerprints.ToBitString()
+        
+    mol_smile_map = sorted(mol_smile_dict.items())
+    molecules = [item[0] for item in mol_smile_map]
+    smiles = [item[1] for item in mol_smile_map]
+    smiles_df = pd.DataFrame({'Molecule': molecules, 'SMILES': smiles})
+    
+    molecules = [key for key,_ in mol_fps_dict.iteritems()]
+    fingerprints = [value for _,value in mol_fps_dict.iteritems()]
+    fingerprints_df = pd.DataFrame({'Molecule': molecules, 'Fingerprints': fingerprints})
+    
+    result = pd.merge(smiles_df, fingerprints_df, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_Pria_Retest, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_Pria_FP, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_Pria_Primary, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_RMI, on='Molecule', how='outer')
+    result = pd.merge(result, Keck_RMI_cdd, on='Molecule', how='outer')
+
+    result.to_csv(output_file_name, index=None)
+
+    return
+
+
+if __name__ == '__main__':
+    transform_data('../dataset/keck_complete.csv')
--- src/function.py
+++ src/function.py
@@ -1,6 +1,127 @@
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import StratifiedKFold
+import random
+import os
+from sklearn.cross_validation import StratifiedKFold, KFold
+
+
+'''
+Analyze the original dataset
+'''
+def analysis(data):
+    class Node:
+        def __init__(self, retest, fp, rmi):
+            self.retest = retest
+            self.fp = fp
+            self.rmi = rmi
+            if not np.isnan(self.rmi):
+                self.rmi = int(self.rmi)
+            else:
+                self.rmi = np.NaN
+            
+        def __str__(self):
+            ret = 'retest: {}, fp: {}, rmi: {}'.format(self.retest, self.fp, self.rmi)
+            return ret
+        
+        def __eq__(self, other):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+        
+        def __hash__(self):
+            return hash(self.retest) ^ hash(self.fp) ^ hash(self.rmi)
+        
+        def __cmp__(self):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+    
+    dict_ = {}
+    for ix, row in data.iterrows():
+        node = Node(row['Keck_Pria_AS_Retest'], row['Keck_Pria_FP_data'], row['Keck_RMI_cdd'])
+        if node not in dict_.keys():
+            dict_[node] = 1
+        else:
+            dict_[node] += 1
+    
+    for k in dict_.keys():
+        print k, '\t---', dict_[k]
+    
+    return
+
+
+'''
+Apply greedy method to split data when merging multi-task
+'''
+def greedy_multi_splitting(data, k, directory, file_list):
+    class Node:
+        def __init__(self, retest, fp, rmi):
+            self.retest = retest
+            self.fp = fp
+            self.rmi = rmi
+            if not np.isnan(self.rmi):
+                self.rmi = int(self.rmi)
+            else:
+                self.rmi = np.NaN
+            
+        def __str__(self):
+            ret = 'retest: {}, fp: {}, rmi: {}'.format(self.retest, self.fp, self.rmi)
+            return ret
+        
+        def __eq__(self, other):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+        
+        def __hash__(self):
+            return hash(self.retest) ^ hash(self.fp) ^ hash(self.rmi)
+        
+        def __cmp__(self):
+            return (self.retest, self.fp, self.rmi) == (other.retest, other.fp, other.rmi)
+    
+    dict_ = {}
+    for ix, row in data.iterrows():
+        node = Node(row['Keck_Pria_AS_Retest'], row['Keck_Pria_FP_data'], row['Keck_RMI_cdd'])
+        if node not in dict_.keys():
+            dict_[node] = []
+        dict_[node].append(ix)
+        
+    list_ = []
+    for key in dict_.keys():
+        one_group_list = np.array(dict_[key])
+        current = []
+
+        if len(one_group_list) < k:
+            n = len(one_group_list)
+            for i in range(n):
+                current.append(np.array(one_group_list[i]))
+            for i in range(n, k):
+                current.append(np.array([]))
+        else:
+            kf = KFold(len(one_group_list), k, shuffle=True)
+            for _, test_index in kf:
+                current.append(one_group_list[test_index])
+        random.shuffle(current)
+        list_.append(current)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        
+    print len(list_)
+
+    for split in range(k):
+        index_block = np.hstack((list_[0][split],
+                                 list_[1][split],
+                                 list_[2][split],
+                                 list_[3][split],
+                                 list_[4][split],
+                                 list_[5][split],
+                                 list_[6][split],
+                                 list_[7][split],
+                                 list_[8][split]))
+        index_block = index_block.astype(np.int)
+        df_block = data.iloc[index_block]
+        print df_block.shape
+
+        file_path = directory + file_list[split]
+        df_block.to_csv(file_path, index=None)
+    
+    return
+
 
 
 '''
@@ -35,7 +156,7 @@ And merged into one dataset
 def read_merged_data(input_file_list):
     whole_pd = pd.DataFrame()
     for input_file in input_file_list:
-        data_pd = pd.read_csv(input_file, dtype=str)
+        data_pd = pd.read_csv(input_file)
         whole_pd = whole_pd.append(data_pd)
     return whole_pd
 
