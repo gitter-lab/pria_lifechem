a77879e55da58a6d971f6759502302f21451836e
Malnammi <alnammi2009@gmail.com>
2017-04-10 11:50:19 -0800

    Update to evaluation.py to install prroc package

--- src/evaluation.py
+++ src/evaluation.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import auc
 from croc import BEDROC, ScoredData
 import os
+from rpy2.robjects.packages import importr
 
 
 '''
@@ -135,6 +136,10 @@ def precision_auc_single(actual, predicted, mode='auc.integral'):
     if mode == 'auc.sklearn':
         prec_auc = average_precision_score(actual, predicted)
     else:
+        utils = importr('utils')
+        utils.chooseCRANmirror(ind=1)
+        utils.install_packages('PRROC')
+        
         prroc = rpackages.importr('PRROC')
         x = robjects.FloatVector(actual)
         y = robjects.FloatVector(predicted)
