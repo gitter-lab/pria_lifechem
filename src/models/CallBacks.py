7c7b5cef4b13bd7c20717752b9302e83120ec9c8
chao1224 <lschacker@hotmail.com>
2017-04-15 21:01:01 -0500

    add multi callbacks

--- src/models/CallBacks.py
+++ src/models/CallBacks.py
@@ -115,3 +115,116 @@ class KeckCallBackOnPrecision(keras.callbacks.Callback):
 
     def get_best_auc(self):
         return self.best_pr
+
+
+
+class CallBackOnROCMulti(keras.callbacks.Callback):
+    def __init__(self, X_train, y_train, X_val, y_val, eval_indices, eval_mean_or_median, patience=0):
+        super(keras.callbacks.Callback, self).__init__()
+        self.curr_auc = 0
+        self.best_auc = 0
+        self.counter = 0
+        self.patience = patience
+        self.best_model = None
+        self.eval_indices = eval_indices
+        self.eval_mean_or_median = eval_mean_or_median
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+
+    def on_train_begin(self, logs={}):
+        self.nb_epoch = self.params['nb_epoch']
+        self.curr_auc = self.get_model_roc_auc(self.y_val, self.model.predict(self.X_val))
+        self.best_auc = self.curr_auc
+        self.best_model = self.model
+
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred_val = self.model.predict(self.X_val)
+        y_pred_train = self.model.predict(self.X_train)
+        self.curr_auc = self.get_model_roc_auc(self.y_val, y_pred_val)
+        if self.curr_auc < self.best_auc:
+            if self.counter >= self.patience:
+                self.model.stop_training = True
+            else:
+                self.counter += 1
+        else:
+            self.counter = 0
+            self.best_auc = self.curr_auc
+            self.best_model = self.model
+        curr_precision = self.get_model_precision_auc(self.y_val, y_pred_val)
+        train_auc = self.get_model_roc_auc(self.y_train, y_pred_train)
+        train_precision = self.get_model_precision_auc(self.y_train, y_pred_train)
+        print('Epoch {}/{}'.format(epoch + 1, self.nb_epoch))
+        print('AUC Train: {} ---- AUC Val: {}'.format(train_auc, self.curr_auc))
+        print('Precision Train: {} ---- Precision Val: {}'.format(train_precision, curr_precision))
+
+    def get_best_model(self):
+        return self.best_model
+
+    def get_best_auc(self):
+        return self.best_auc
+
+    def get_model_roc_auc(self, true_label, predicted_label):
+        return roc_auc(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
+
+    def get_model_precision_auc(self, true_label, predicted_label):
+        return precision_auc(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
+
+
+class CallBackOnPRMulti(keras.callbacks.Callback):
+    # define custom classes
+    # following class is used for keras to compute the precision each epoch
+    # and do early stoppping based on that
+    def __init__(self, X_train, y_train, X_val, y_val, eval_indices, eval_mean_or_median, patience=0):
+        super(keras.callbacks.Callback, self).__init__()
+        self.curr_precision = 0
+        self.best_precision = 0
+        self.counter = 0
+        self.patience = patience
+        self.best_model = None
+        self.eval_indices = eval_indices
+        self.eval_mean_or_median = eval_mean_or_median
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+
+    def on_train_begin(self, logs={}):
+        self.nb_epoch = self.params['nb_epoch']
+        self.curr_precision = self.get_model_precision_auc(self.y_val, self.model.predict(self.X_val))
+        self.best_precision = self.curr_precision
+        self.best_model = self.model
+
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred_val = self.model.predict(self.X_val)
+        y_pred_train = self.model.predict(self.X_train)
+        self.curr_precision = self.get_model_precision_auc(self.y_val, y_pred_val)
+        if self.curr_precision < self.best_precision:
+            if self.counter >= self.patience:
+                self.model.stop_training = True
+            else:
+                self.counter += 1
+        else:
+            self.counter = 0
+            self.best_precision = self.curr_precision
+            self.best_model = self.model
+
+        train_precision = self.get_model_precision_auc(self.y_train, y_pred_train)
+        train_auc = self.get_model_roc_auc(self.y_train, y_pred_train)
+        curr_auc = self.get_model_roc_auc(self.y_val, y_pred_val)
+        print('Epoch {}/{}'.format(epoch + 1, self.nb_epoch))
+        print('Precision Train: {} ---- Precision Val: {}'.format(train_precision, self.curr_precision))
+        print('AUC Train: {} ---- AUC Val: {}'.format(train_auc, curr_auc))
+
+    def get_best_model(self):
+        return self.best_model
+
+    def get_best_precision(self):
+        return self.best_precision
+
+    def get_model_roc_auc(self, true_label, predicted_label):
+        return roc_auc(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
+
+    def get_model_precision_auc(self, true_label, predicted_label):
+        return precision_auc(true_label, predicted_label, self.eval_indices, self.eval_mean_or_median)
