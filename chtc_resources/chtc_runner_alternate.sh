88071883cd6fee1948d38ef7e2e95413ef53c279
Malnammi <alnammi2009@gmail.com>
2017-04-15 17:10:00 -0800

    Multiple Updates
    -added alternate chtc runner script
    
    -added pcba fold creation and merging with keck functions in function.py
    
    -added hash checks for pcba and keck_pcba folds in integrity_checker.py

---
+++ chtc_resources/chtc_runner_alternate.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+mkdir job_results
+mkdir job_results/deep_chem
+mkdir job_results/irv
+mkdir job_results/light_chem
+mkdir job_results/neural_networks
+mkdir job_results/random_forest
+
+wget -q â€“retry-connrefused â€“waitretry=10 http://proxy.chtc.wisc.edu/SQUID/alnammi/Anaconda2-4.3.1-Linux-x86_64.sh #here I get the anaconda file from squid
+wget -r -nH --cut-dirs=2 -np -R index.html* -q â€“retry-connrefused â€“waitretry=10 http://proxy.chtc.wisc.edu/SQUID/alnammi/dataset/ #here I get the dataset 
+
+chmod 777 * #wget does strange things
+
+echo 'Done getting from squid'
+
+./Anaconda2-4.3.1-Linux-x86_64.sh -b -p ./anaconda > /dev/null #install anaconda, I also add an argument to the directory name
+
+export PATH=$PWD/anaconda/bin:$PATH
+
+echo 'Done installing anaconda'
+chmod 777 *
+
+#keras stuff
+conda install --yes pyyaml > /dev/null
+conda install --yes HDF5 > /dev/null
+conda install --yes h5py > /dev/null
+conda install --yes -c rdonnelly libgpuarray > /dev/null
+conda install --yes -c rdonnelly pygpu > /dev/null
+conda install --yes -c rdonnelly theano > /dev/null
+conda install --yes -c conda-forge keras=1.2* > /dev/null
+conda install --yes scikit-learn=0.17* > /dev/null
+conda install --yes pandas > /dev/null
+conda install --yes -c rdkit rdkit-postgresql > /dev/null
+conda install --yes -c r rpy2 > /dev/null
+conda install --yes -c bioconda r-prroc=1.1 > /dev/null
+conda install --yes -c auto croc=1.0.63 > /dev/null
+
+echo 'Done installing libraries'
+
+chmod 777 -R ./anaconda
+
+#get virtual-screening from github
+curl -H "Authorization: token 5879e760ce2f7b753aa80bda34811162ec7ababe" -L https://api.github.com/repos/lscHacker/virtual-screening/zipball > virtual-screening-master.zip
+unzip virtual-screening-master.zip > /dev/null
+rm -f virtual-screening-master.zip
+
+mv lsc* virtual-screening
+rm -rf ./virtual-screening/dataset
+mv ./dataset/ ./virtual-screening/dataset/
+export PYTHONPATH=${PYTHONPATH}:${_CONDOR_JOB_IWD}/virtual-screening:${_CONDOR_JOB_IWD}/virtual-screening/src:${_CONDOR_JOB_IWD}/virtual-screening/src/models
+
+cd virtual-screening
+
+
+#run python job
+python_jobs_dir=$1
+KERAS_BACKEND=theano THEANO_FLAGS="base_compiledir=./tmp,floatX=float32,device=cuda,gpuarray.preallocate=0.8" python src/chtc_distributor.py $python_jobs_dir
+
+echo 'Done running job'
+
+#THEANO_FLAGS="dnn.library_path=/usr/local/cuda-7.5/lib64,dnn.include_path=/usr/local/cuda-7.5/include,base_compiledir=./tmp,floatX=float32,device=cuda,gpuarray.preallocate=0.8" python -c 'import theano; print(theano.config)' | less
+
+cd ..
+
+#clean up everything I don't want transfered back
+rm -f Anaconda*
+rm -R -f virtual-screening*
+rm -rf ./anaconda*
+
+
+echo _CONDOR_JOB_IWD $_CONDOR_JOB_IWD
+echo Cluster $cluster
+echo Process $process
+echo RunningOn $runningon
--- src/integrity_checker.py
+++ src/integrity_checker.py
@@ -12,24 +12,29 @@ Created on Sat Mar 25 07:41:32 2017
  
  Usage: Pass this function the directory containing the fold files. 
         Currently only supports 3, 4, and 5 folds via hard-coded hash keys.
+        
+     dataset_name has three options 'keck', 'pcba', and 'keck_pcba'
 """
 import hashlib
+import re
 import os
 
-def check_keck_fold_integrity(fold_directory):
+def check_keck_fold_integrity(fold_directory, dataset_name='keck'):
     #Add to this dictionary to add more fold options    
     switch = {3 : fold_3_keys,
               4 : fold_4_keys,
               5 : fold_5_keys
               } 
-              
+    
+    regex = re.compile('file_(.*).csv')
     file_list = os.listdir(fold_directory)
+    file_list = [file_name for file_name in file_list if regex.search(file_name)]
     file_list = sorted(file_list)
     k = len(file_list)
     key_hash_list = None
     
     try:
-        key_hash_list = switch[k]()
+        key_hash_list = switch[k](dataset_name)
     except KeyError:
         print("Number of folds is not supported. Check for repo updates.")
         return
@@ -50,20 +55,53 @@ def check_keck_fold_integrity(fold_directory):
         print("There are errors in the files")
         
 
-def fold_3_keys():
-    return [('file_0.csv', b'\xc2)4\x85\x8f\xf5\x17Wm\xcf\xbbG\x9a\x81\x03\xa6'),
-     ('file_1.csv', b'\xdd<M\xf1\xf43+\xe5\xf7\xa6\xc3:\xf2X5\xca'),
-     ('file_2.csv', b'\x93r\xa8\xd0\x8d\xecS\x058j\xc0\x05\x04<\xae\xba')]
+def fold_3_keys(dataset_name='keck'):
+    hash_dict = {
+    'keck':[('file_0.csv', b'\xc2)4\x85\x8f\xf5\x17Wm\xcf\xbbG\x9a\x81\x03\xa6'),
+             ('file_1.csv', b'\xdd<M\xf1\xf43+\xe5\xf7\xa6\xc3:\xf2X5\xca'),
+             ('file_2.csv', b'\x93r\xa8\xd0\x8d\xecS\x058j\xc0\x05\x04<\xae\xba')],
+    'pcba':[('file_0.csv', b'\xec\x82jO\xfc\xf9\xda\xbcc\x07F\x88\xe3\xe8\xcf\xb6'),
+             ('file_1.csv', b'\xa2l\x8e\x9e\xc9\xdb\xe54+*\xc91\xc7j|\x91'),
+             ('file_2.csv', b'y:\xa6F\xafL\xf5\xe2q\x13\x14\xea7W\xa0\x9b')],
+    'keck_pcba':[('file_0.csv', b'3\x8b\xc9\xc5\xdd\xf0XH\x8b2!\x8d>\xbf\xea\xca'),
+                 ('file_1.csv', b'\x1a\xa8\xef\xe3\x11\x14\xcdO\xf9M>\xdc\x89\xaa\x84_'),
+                 ('file_2.csv', b'n\x87\xbc\xc3{\xe0\x0e;y=#\x82[t\x9a\xba')]
+    }
+    return hash_dict[dataset_name]
     
-def fold_4_keys():
-    return [('file_0.csv', b'\xd5\x01\x9d\x9eC9\xba\xc1\x08\xb9\x7fjp\x02; '),
-     ('file_1.csv', b'\x9e\x04\xce\xca\x92\xc8\xc5J\x81\xb2m\xc6)\x16?\x9a'),
-     ('file_2.csv', b'W\xd9\x1c\x16R\x06\xe8\xba\xe9$\xdew\xe3\x01\xe8E'),
-     ('file_3.csv', b'q\x02\xee\xc7x,\xb4|"L\x10\x98EI\xd1\xfe')]
+def fold_4_keys(dataset_name='keck'):
+    hash_dict = {
+    'keck':[('file_0.csv', b'\xd5\x01\x9d\x9eC9\xba\xc1\x08\xb9\x7fjp\x02; '),
+             ('file_1.csv', b'\x9e\x04\xce\xca\x92\xc8\xc5J\x81\xb2m\xc6)\x16?\x9a'),
+             ('file_2.csv', b'W\xd9\x1c\x16R\x06\xe8\xba\xe9$\xdew\xe3\x01\xe8E'),
+             ('file_3.csv', b'q\x02\xee\xc7x,\xb4|"L\x10\x98EI\xd1\xfe')],
+    'pcba':[('file_0.csv', b'o\xba>Gi\xfc\xf4lJ\x9b\x1bx\xc10x+'),
+             ('file_1.csv', b'">\xe1\xce\xcc\xfa8*#}\x07T\x02b\x87\xc5'),
+             ('file_2.csv', b'\xce\xe6\xa3\x9e8Ar\x88c\xfd3\xfaK\xe0\xd1b'),
+             ('file_3.csv', b'|\xcc:t\xe4\xcb\x9a\xaf\xeb\xcf\x0e{\x06\xdb\xd8I')],
+    'keck_pcba':[('file_0.csv', b'`\xf5N\xce2\rz.\xab}\n\x81sr\x02\x10'),
+                 ('file_1.csv', b'\x1d\xfd\x95@\x87\x8c+\x9f\x00\x80\xc3-z<\xabl'),
+                 ('file_2.csv', b'u6\xb6\x1b\x7fs\x87}n\xd6h\x1d|g\xc8\x90'),
+                 ('file_3.csv', b'\x14\xc2\xb6\x84i\x98g\x98dx\x00\x88\xc3\x0emV')]
+    }
+    return hash_dict[dataset_name]
 
-def fold_5_keys():
-    return [('file_0.csv', b'\xfb\x97\xd0\xdcs\xd5\x12^\xfc\x96\xef1\\\x03\x0e\n'),
-     ('file_1.csv', b'\x11F\xf9\xa3v2\xcaW\xf0\xb5\xc8\x1dZ\xae\x0c\xb6'),
-     ('file_2.csv', b'\x05\xfd\t^\x18_M\xcaJ\xc6\x81\xc8PE\xed\x06'),
-     ('file_3.csv', b'\xaa0LG\xc3\xe0\xe1y\xf0\xe1G\x84\xc9uz\xed'),
-     ('file_4.csv', b'\x94\xf1\xdfw\x81\x89/,\x94\x12\x9f\x9c\xc1\xc2.\xe8')]
+def fold_5_keys(dataset_name='keck'):
+    hash_dict = {
+    'keck':[('file_0.csv', b'\xfb\x97\xd0\xdcs\xd5\x12^\xfc\x96\xef1\\\x03\x0e\n'),
+             ('file_1.csv', b'\x11F\xf9\xa3v2\xcaW\xf0\xb5\xc8\x1dZ\xae\x0c\xb6'),
+             ('file_2.csv', b'\x05\xfd\t^\x18_M\xcaJ\xc6\x81\xc8PE\xed\x06'),
+             ('file_3.csv', b'\xaa0LG\xc3\xe0\xe1y\xf0\xe1G\x84\xc9uz\xed'),
+             ('file_4.csv', b'\x94\xf1\xdfw\x81\x89/,\x94\x12\x9f\x9c\xc1\xc2.\xe8')],
+    'pcba':[('file_0.csv', b'wh\x81^:{\xcb\x11V\xa8\xfcf\xc0\xdf\x08\x8d'),
+             ('file_1.csv', b'p\x99\xcc\xe1D\xc2\xafA\x14\xa2\x00\xc6\xa2\x7f\xc6\xce'),
+             ('file_2.csv', b'\xe6\x9f\x8d\x0f\x88\xc6\xb0Fd\xb7\xa4\x92d\nwu'),
+             ('file_3.csv', b')\xf1\xd0@\x1f\t\xdd\xa5\x93\xc3\n\x9c&\x90\xe6E'),
+             ('file_4.csv', b'\xb8\x93\xe7\x851\xef5\xe9\xc3r`\x95\r\xf5\xc8\xa8')],
+    'keck_pcba':[('file_0.csv', b'}\xddy\x8c\xcc"J\\1\xbf\x1b1\x9d\xdf\x9c\x11'),
+                 ('file_1.csv', b'\x80\x9b)\x9fz3\x83\x02\r1,D\xadE"\x82'),
+                 ('file_2.csv', b'\xd1N\xf8\'\xec\xc6\xbe\x9a"\x7f\x7f$\xec\x12\xacs'),
+                 ('file_3.csv', b'\xe3\x1a\xd8\xf2\x9b?\xa1\xdfE\x1c?\x94F0\x8e\xab'),
+                 ('file_4.csv', b'\xebB\xaf\x17B\xe5xXp\x16\xf1kh\x82\xd9\x1b')]
+    }
+    return hash_dict[dataset_name]
