ad17a8510df29fab976c401ba47c7fa1fe5140f7
chao1224 <sliu426@wisc.edu>
2017-04-18 14:31:47 -0500

    Add project url

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='virtual_screening',
       version='0.1',
-      description='virtual screening',
-      url='https://github.com/chao1224/virtual-screening',
-      author='Shengchao Liu',
+      description='virtual screening for drug discovery, deep learning based on Keras',
+      url='https://research.wisc.edu/funding/uw2020/round-3-projects/an-adaptive-computational-pipeline-drug-discovery/',
+      author='Shengchao Liu, Moayad Alnammi, Gitter Lab',
       author_email='shengchao@cs.wisc.edu',
       license='GPL v3',
       packages=['virtual_screening'],
