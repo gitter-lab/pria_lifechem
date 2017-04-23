369d79976e531912a8f687cafff69888da4a2be3
chao1224 <sliu426@wisc.edu>
2017-04-23 01:24:36 -0500

    debugging

--- chtc_resources/single_regression_grid_search.sh
+++ chtc_resources/single_regression_grid_search.sh
@@ -54,7 +54,7 @@ date
 KERAS_BACKEND=theano \
 THEANO_FLAGS="base_compiledir=./tmp,floatX=float32,gpuarray.preallocate=0.8" \
 python grid_search_optimization.py \
---config_json_file=../../json/single_classification.json \
+--config_json_file=../../json/single_regression.json \
 --PMTNN_weight_file=$_CONDOR_JOB_IWD/$transfer_output_files/$process.weight \
 --config_csv_file=$_CONDOR_JOB_IWD/$transfer_output_files/$process.result.csv \
 --SMILES_mapping_json_file=../../json/SMILES_mapping.json \
