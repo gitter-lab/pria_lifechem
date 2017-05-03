8a8f2cc8e0ffc83375ddd89899418b774c34f7b7
chao1224 <lschacker@hotmail.com>
2017-05-03 01:54:36 -0500

    Add supprot functions for cross_validation

--- virtual_screening/analysis/analysis.py
+++ virtual_screening/analysis/analysis.py
@@ -2,6 +2,8 @@ import re
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 evaluations = {0: 'train prec', 1: 'train roc', 2: 'train bedroc',
@@ -72,6 +74,86 @@ def action(dir_name, k):
             EF_01_list.append([0, 0, 0])
             continue
             
+        with open(file_path, 'r') as f:
+            read_data = f.readlines()
+            for line in read_data:
+                if 'train precision' in line:
+                    train_prec_list.append(get_number(line))
+                if 'train roc' in line:
+                    train_roc_list.append(get_number(line))
+                if 'train bedroc' in line:
+                    train_bedroc_list.append(get_number(line))
+                if 'validation precision' in line:
+                    val_prec_list.append(get_number(line))
+                if 'validation roc' in line:
+                    val_roc_list.append(get_number(line))
+                if 'validation bedroc' in line:
+                    val_bedroc_list.append(get_number(line))
+                if 'test precision' in line:
+                    test_prec_list.append(get_number(line))
+                if 'test roc' in line:
+                    test_roc_list.append(get_number(line))
+                if 'test bedroc' in line:
+                    test_bedroc_list.append(get_number(line))
+                if 'ratio: 0.02,' in line:
+                    EF_2_list.append(get_EF_number(line))
+                if 'ratio: 0.01,' in line:
+                    EF_1_list.append(get_EF_number(line))
+                if 'ratio: 0.0015,' in line:
+                    EF_015_list.append(get_EF_number(line))
+                if 'ratio: 0.001,' in line:
+                    EF_01_list.append(get_EF_number(line))
+
+    train_prec_list = np.array(train_prec_list)
+    train_roc_list = np.array(train_roc_list)
+    train_bedroc_list = np.array(train_bedroc_list)
+
+    val_prec_list = np.array(val_prec_list)
+    val_roc_list = np.array(val_roc_list)
+    val_bedroc_list = np.array(val_bedroc_list)
+
+    test_prec_list = np.array(test_prec_list)
+    test_roc_list = np.array(test_roc_list)
+    test_bedroc_list = np.array(test_bedroc_list)
+
+    EF_2_list = np.array(EF_2_list)
+    EF_1_list = np.array(EF_1_list)
+    EF_015_list = np.array(EF_015_list)
+    EF_01_list = np.array(EF_01_list)
+
+    return train_prec_list, train_roc_list, train_bedroc_list, \
+           val_prec_list, val_roc_list, val_bedroc_list, \
+           test_prec_list, test_roc_list, test_bedroc_list, \
+           EF_2_list, EF_1_list, EF_015_list, EF_01_list
+
+
+'''
+Simply ignore the non-existing files
+'''
+def action_ignore(dir_name, k):
+    train_prec_list = []
+    train_roc_list = []
+    train_bedroc_list = []
+
+    val_prec_list = []
+    val_roc_list = []
+    val_bedroc_list = []
+
+    test_prec_list = []
+    test_roc_list = []
+    test_bedroc_list = []
+
+    EF_2_list = []
+    EF_1_list = []
+    EF_015_list = []
+    EF_01_list = []
+
+    for x in range(k):
+        file_path = dir_name + '{}.out'.format(x)
+
+        if not os.path.isfile(file_path):
+            continue
+
         with open(file_path, 'r') as f:
             read_data = f.readlines()
             for line in read_data:
@@ -273,5 +355,99 @@ def get_ranked_analysis(dir_path, k, evaluation_list, fetch_top_num):
 
         print
 
+
+    return
+
+
+def fetch_one_model(dir_path, number, evaluation_list, model):
+    train_prec_list, train_roc_list, train_bedroc_list, \
+    val_prec_list, val_roc_list, val_bedroc_list, \
+    test_prec_list, test_roc_list, test_bedroc_list, \
+    EF_2_list, EF_1_list, EF_015_list, EF_01_list = action_ignore(dir_path, number)
+
+    evaluation_column = []
+    value_column = []
+    model_column = []
+
+    if 'train prec' in evaluation_list:
+        evaluation_column.extend(['train prec' for _ in train_prec_list])
+        value_column.extend(list(train_prec_list))
+        model_column.extend([model for _ in train_prec_list])
+    if 'train roc' in evaluation_list:
+        evaluation_column.extend(['train roc' for _ in train_roc_list])
+        value_column.extend(list(train_roc_list))
+        model_column.extend([model for _ in train_roc_list])
+    if 'train bedroc' in evaluation_list:
+        evaluation_column.extend(['train bedroc' for _ in train_bedroc_list])
+        value_column.extend(list(train_bedroc_list))
+        model_column.extend([model for _ in train_bedroc_list])
+
+    if 'validation prec' in evaluation_list:
+        evaluation_column.extend(['validation prec' for _ in val_prec_list])
+        value_column.extend(list(val_prec_list))
+        model_column.extend([model for _ in val_prec_list])
+    if 'validation roc' in evaluation_list:
+        evaluation_column.extend(['validation roc' for _ in val_roc_list])
+        value_column.extend(list(val_roc_list))
+        model_column.extend([model for _ in val_roc_list])
+    if 'validation bedroc' in evaluation_list:
+        evaluation_column.extend(['validation bedroc' for _ in val_bedroc_list])
+        value_column.extend(list(val_bedroc_list))
+        model_column.extend([model for _ in val_bedroc_list])
+
+    if 'test prec' in evaluation_list:
+        evaluation_column.extend(['test prec' for _ in test_prec_list])
+        value_column.extend(list(test_prec_list))
+        model_column.extend([model for _ in test_prec_list])
+    if 'test roc' in evaluation_list:
+        evaluation_column.extend(['test roc' for _ in test_prec_list])
+        value_column.extend(list(test_prec_list))
+        model_column.extend([model for _ in test_prec_list])
+    if 'test bedroc' in evaluation_list:
+        evaluation_column.extend(['test bedroc' for _ in test_prec_list])
+        value_column.extend(list(test_prec_list))
+        model_column.extend([model for _ in test_prec_list])
+
+    if 'ratio: 0.02' in evaluation_list:
+        evaluation_column.extend(['ratio: 0.02' for _ in EF_2_list])
+        value_column.extend(list(EF_2_list))
+        model_column.extend([model for _ in EF_2_list])
+    if 'ratio: 0.01' in evaluation_list:
+        evaluation_column.extend(['ratio: 0.01' for _ in EF_1_list])
+        value_column.extend(list(EF_1_list))
+        model_column.extend([model for _ in EF_1_list])
+    if 'ratio: 0.0015' in evaluation_list:
+        evaluation_column.extend(['ratio: 0.0015' for _ in EF_015_list])
+        value_column.extend(list(EF_015_list))
+        model_column.extend([model for _ in EF_015_list])
+    if 'ratio: 0.001' in evaluation_list:
+        evaluation_column.extend(['ratio: 0.001' for _ in EF_01_list])
+        value_column.extend(list(EF_01_list))
+        model_column.extend([model for _ in EF_01_list])
+
+    return evaluation_column, value_column, model_column
+
+
+def plot_cross_validation(dir_path_list, evaluation_list, model_list):
+    evaluation_column = []
+    value_column = []
+    model_column = []
+
+    for i in range(len(dir_path_list)):
+        dir_ = dir_path_list[i]
+        model = model_list[i]
+
+        c1, c2, c3 = fetch_one_model(dir_, 20, evaluation_list=evaluation_list, model=model)
+        evaluation_column.extend(c1)
+        value_column.extend(c2)
+        model_column.extend(c3)
+
+    data_pd = pd.DataFrame({'evaluation method': evaluation_column,
+                            'value': value_column,
+                            'model': model_column})
+
+    sns.boxplot(x="evaluation method", y="value", hue="model", data=data_pd, palette="PRGn")
+    sns.despine(offset=20, trim=True)
+    sns.plt.rcParams['figure.figsize'] = (10.0, 5.0)
 
     return
