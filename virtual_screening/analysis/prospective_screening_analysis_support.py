640fd4c030f2970db92bce3ae7da61a43bb45249
chao1224 <lschacker@hotmail.com>
2017-11-08 17:54:09 -0600

    Clean prospective screening results, and merge into one file

---
+++ virtual_screening/analysis/prospective_screening_analysis_support.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import os
+from prospective_screening_model_names import model_name_mapping
+
+
+if __name__ == '__main__':
+    dataframe = pd.read_excel('../../output/stage_2_predictions/Keck_LC4_export.xlsx')
+
+    supplier_id = dataframe['Supplier ID'].tolist()
+    failed_id = ['F0401-0050', 'F2964-1411', 'F2964-1523']
+    inhibits = dataframe[
+        'PriA-SSB AS, normalized for plate and edge effects, correct plate map: % inhibition Alpha, normalized (%)'].tolist()
+
+    positive_enumerate = filter(lambda x: x[1] >= 35 and supplier_id[x[0]] not in failed_id, enumerate(inhibits))
+    positive_idx = map(lambda x: x[0], positive_enumerate)
+    actual_label = map(lambda x: 1 if x in positive_idx else 0, range(len(supplier_id)))
+
+    complete_df = pd.DataFrame({'molecule id': supplier_id, 'actual label': actual_label})
+    complete_df = complete_df[['molecule id', 'actual label']]
+    # complete_df[complete_df['actual label'] > 0]
+
+    dir_ = '../../output/stage_2_predictions/Keck_Pria_AS_Retest'
+
+    molecule_id = None
+
+    for model_name in model_name_mapping.keys():
+        file_path = '{}/{}.npz'.format(dir_, model_name)
+        if not os.path.exists(file_path):
+            continue
+        print 'model: {} exists'.format(model_name)
+        data = np.load(file_path)
+
+        if 'irv' in model_name or 'random_forest' in model_name:
+            y_pred = data['y_pred_on_test'][:, 0]
+        else:
+            y_pred = data['y_pred']
+            molecule_id = data['molecule_id']
+
+        if y_pred.ndim == 2:
+            y_pred = y_pred[:, 0]
+
+        temp_df = pd.DataFrame({'molecule id': molecule_id,
+                                model_name_mapping[model_name]: y_pred})
+        if model_name in complete_df.columns:
+            continue
+        complete_df = complete_df.join(temp_df.set_index('molecule id'), on='molecule id')
+        print
+
+    complete_df.to_csv('{}/complete.csv'.format(dir_), index=None)
---
+++ virtual_screening/analysis/prospective_screening_model_names.py
@@ -0,0 +1,51 @@
+model_name_mapping = {
+    # dnn
+    'single_regression_2': 'SingleRegression_a',
+    'single_regression_11': 'SingleRegression_b',
+    'single_classification_22': 'SingleClassification_a',
+    'single_classification_42': 'SingleClassification_b',
+    'multi_classification_15': 'MultiClassification_a',
+    'multi_classification_18': 'MultiClassification_b',
+    'vanilla_lstm_8': 'LSTM_a',
+    'vanilla_lstm_19': 'LSTM_b',
+
+    # random forest
+    'random_forest_12': 'RandomForest_a',
+    'random_forest_13': 'RandomForest_b',
+    'random_forest_14': 'RandomForest_c',
+    'random_forest_24': 'RandomForest_d',
+    'random_forest_25': 'RandomForest_e',
+    'random_forest_96': 'RandomForest_f',
+    'random_forest_97': 'RandomForest_g',
+
+    # irv
+    'irv_5': 'IRV_a',
+    'irv_10': 'IRV_b',
+    'irv_20': 'IRV_c',
+    'irv_40': 'IRV_d',
+    'irv_80': 'IRV_e',
+
+    # docking
+    'dockscore_hybrid': 'Docking_hybrid',
+    'dockscore_fred': 'Docking_fred',
+    'dockscore_dock6': 'Docking_dock6',
+    'dockscore_rdockint': 'Docking_rdockint',
+    'dockscore_rdocktot': 'Docking_rdocktot',
+    'dockscore_surflex': 'Docking_surflex',
+    'dockscore_ad4': 'Docking_ad4',
+    'dockscore_plants': 'Docking_plants',
+    'dockscore_smina': 'Docking_smina',
+    'consensus_dockscore_max': 'ConsensusDocking_max',
+    'consensus_bcs_efr1_opt': 'ConsensusDocking_efr1_opt',
+    'consensus_bcs_rocauc_opt': 'ConsensusDocking_rocauc_opt',
+    'consensus_dockscore_median': 'ConsensusDocking_median',
+    'consensus_dockscore_mean': 'ConsensusDocking_mean',
+
+    # lightchemPRAUC_1Layer1Model
+    'lightchem_PRAUC_1Layer1Model_test_lc4': 'LightChem_a',
+    'lightchem_PRAUC_5Layer1Model_test_lc4': 'LightChem_b',
+    'lightchem_PRAUC_10Layer1Model_test_lc4': 'LightChem_c',
+    'lightchem_ROCAUC_1Layer1Model_test_lc4': 'LightChem_d',
+    'lightchem_ROCAUC_5Layer1Model_test_lc4': 'LightChem_e',
+    'lightchem_ROCAUC_10Layer1Model_test_lc4': 'LightChem_f',
+}
