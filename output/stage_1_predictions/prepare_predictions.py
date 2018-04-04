92d9f9463c55f387ca902968c02b5f84fcafd979
chao1224 <shengchao.hacker@gmail.com>
2018-04-04 13:01:31 -0500

    Updates: - Use single quote - Move docking output

--- output/stage_1_predictions/prepare_predictions.py
+++ output/stage_1_predictions/prepare_predictions.py
@@ -21,20 +21,20 @@ model_name_list = [
     'deepchem_irv_20',
     'deepchem_irv_40',
     'deepchem_irv_80',
-    "dockscore_hybrid",
-    "dockscore_fred",
-    "dockscore_dock6",
-    "dockscore_rdockint",
-    "dockscore_rdocktot",
-    "dockscore_surflex",
-    "dockscore_ad4",
-    "dockscore_plants",
-    "dockscore_smina",
-    "consensus_dockscore_max",
-    "consensus_bcs_efr1_opt",
-    "consensus_bcs_rocauc_opt",
-    "consensus_dockscore_median",
-    "consensus_dockscore_mean"
+    'dockscore_hybrid',
+    'dockscore_fred',
+    'dockscore_dock6',
+    'dockscore_rdockint',
+    'dockscore_rdocktot',
+    'dockscore_surflex',
+    'dockscore_ad4',
+    'dockscore_plants',
+    'dockscore_smina',
+    'consensus_dockscore_max',
+    'consensus_bcs_efr1_opt',
+    'consensus_bcs_rocauc_opt',
+    'consensus_dockscore_median',
+    'consensus_dockscore_mean'
 ]
 
 fold_num = [
@@ -43,27 +43,6 @@ fold_num = [
     5, 5, 5, 5, 5,
     5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5
 ]
-
-model_name_list = [
-    "dockscore_hybrid",
-    "dockscore_fred",
-    "dockscore_dock6",
-    "dockscore_rdockint",
-    "dockscore_rdocktot",
-    "dockscore_surflex",
-    "dockscore_ad4",
-    "dockscore_plants",
-    "dockscore_smina",
-    "consensus_dockscore_max",
-    "consensus_bcs_efr1_opt",
-    "consensus_bcs_rocauc_opt",
-    "consensus_dockscore_median",
-    "consensus_dockscore_mean"
-]
-
-fold_num = [5, 5, 5, 5, 5,
-            5, 5, 5, 5, 5,
-            5, 5, 5, 5]
 
 task_list = ['cross_validation_Keck_Pria_AS_Retest', 'cross_validation_Keck_FP', 'cross_validation_RMI']
 
