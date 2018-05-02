3f36106f97800a84938fb384bb60ef5230cf9a8b
Anthony Gitter <gitter@biostat.wisc.edu>
2018-05-02 10:51:22 -0500

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='virtual_screening',
       version='0.1',
-      description='virtual screening for drug discovery, deep learning based on Keras',
+      description='Virtual screening for drug discovery, deep learning based on Keras',
       url='https://github.com/chao1224/virtual-screening',
-      author='Shengchao Liu, Moayad Alnammi, Gitter Lab',
+      author='Shengchao Liu, Moayad Alnammi',
       author_email='shengchao@cs.wisc.edu',
-      license='GPL v3',
+      license='MIT',
       packages=['virtual_screening'],
       zip_safe=False)
