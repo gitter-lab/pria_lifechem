16ee369af33f255434094e0919d1f5d9919cb95d
Anthony Gitter <gitter@biostat.wisc.edu>
2018-05-05 06:38:31 -0500

    Update URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='pria_lifechem',
       version='0.1',
       description='Virtual screening for drug discovery, deep learning based on Keras',
-      url='https://github.com/chao1224/pria_lifechem',
+      url='https://github.com/gitter-lab/pria_lifechem',
       author='Shengchao Liu, Moayad Alnammi',
       author_email='shengchao@cs.wisc.edu',
       license='MIT',
