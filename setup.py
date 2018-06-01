69fd18d918166809fd3b678d291a5b9b1e9403f3
Anthony Gitter <gitter@biostat.wisc.edu>
2018-06-01 15:24:17 -0500

    Add patch version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pria_lifechem',
-      version='0.1',
+      version='0.1.0',
       description='Virtual screening for drug discovery, deep learning based on Keras',
       url='https://github.com/gitter-lab/pria_lifechem',
       author='Shengchao Liu, Moayad Alnammi',
