e1b0e930f2563299ffb7707b64c4f5b357b7dc83
chao1224 <shengchao.hacker@gmail.com>
2018-08-02 12:27:55 -0500

    Update Keck AS VS RMI CDD

--- pria_lifechem/analysis/prospective_screening_Keck_AS_VS_RMI_CDD.py
+++ pria_lifechem/analysis/prospective_screening_Keck_AS_VS_RMI_CDD.py
@@ -9,45 +9,48 @@ from prospective_screening_model_names import *
 from prospective_screening_metric_names import *
 
 
-complete_df = pd.read_csv('LC4_complete.csv')
-column_names = ['molecule name', 'molecule id', 'label', 'inhibition']
 
-dir_ = '../../output/stage_2_predictions/RMI'
+dataframe = pd.read_csv('../../dataset/fixed_dataset/pria_prospective.csv.gz')
+molecule_ids = dataframe['Molecule'].tolist()
+actual_labels = dataframe['Keck_Pria_AS_Retest'].tolist()
+inhibits = dataframe['Keck_Pria_Continuous'].tolist()
 
-file_path = '{}/{}.npz'.format(dir_, 'vanilla_lstm_19')
-data = np.load(file_path)
-molecule_id = data['molecule_id']
+complete_df = pd.DataFrame({'molecule': molecule_ids, 'label': actual_labels, 'inhibition': inhibits})
 
+column_names = ['molecule', 'label', 'inhibition']
+complete_df = complete_df[column_names]
+
+dir_ = '../../output/stage_2_predictions/Keck_Pria_AS_Retest'
 model_names = []
-special_models = ['irv', 'random_forest', 'dockscore', 'consensus', 'baseline']
 
 for model_name in model_name_mapping.keys():
     file_path = '{}/{}.npz'.format(dir_, model_name)
     if not os.path.exists(file_path):
+        print('model: {} doesn\'t exist'.format(model_name))
         continue
     data = np.load(file_path)
+    # print(file_path, '\t', data.keys(), '\t', data['y_pred_on_test'].shape)
 
-    if any(x in model_name for x in special_models):
-        y_pred = data['y_pred_on_test']
-    else:
-        y_pred = data['y_pred']
+    y_pred = data['y_pred_on_test'][:, 2]
     if y_pred.ndim == 2:
         y_pred = y_pred[:, 0]
+    print(y_pred.shape, y_pred[:5])
 
-    temp_df = pd.DataFrame({'molecule id': molecule_id,
-                            model_name_mapping[model_name]: y_pred})
-
-    model_names.append(model_name_mapping[model_name])
-    complete_df = complete_df.join(temp_df.set_index('molecule id'), on='molecule id')
+    model_name = model_name_mapping[model_name]
+    model_names.append(model_name)
+    complete_df[model_name] = y_pred
 
 model_names = sorted(model_names)
 column_names.extend(model_names)
 
 complete_df = complete_df[column_names]
+print()
 
 ### Generate Metric DF
 true_label = complete_df['label'].as_matrix()
 true_label = reshape_data_into_2_dim(true_label)
+
+model_names.remove('Baseline')
 
 roc_auc_list = []
 metric_df = pd.DataFrame({'Model': model_names})
@@ -60,6 +63,7 @@ for (metric_name, metric_) in metric_name_mapping.iteritems():
         pred = reshape_data_into_2_dim(pred)
 
         actual, pred = collectively_drop_nan(true_label, pred)
+        print(actual.shape, '\t', pred.shape)
         value = metric_['function'](actual, pred, **metric_['argument'])
         metric_values.append(value)
     metric_df[metric_name] = metric_values
