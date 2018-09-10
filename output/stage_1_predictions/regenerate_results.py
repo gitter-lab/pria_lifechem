592d93ffccaab808727fea54165ce0f944a9caf3
chao1224 <shengchao.hacker@gmail.com>
2018-09-10 11:48:02 -0500

    Update regereation scripts for CV

---
+++ output/stage_1_predictions/regenerate_results.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pria_lifechem
+import os
+from pria_lifechem.function import *
+from pria_lifechem.models.CallBacks import *
+from pria_lifechem.models.deep_classification import *
+from pria_lifechem.models.deep_regression import *
+
+
+fold_num = 20
+task_list = ['cross_validation_Keck_Pria_AS_Retest', 'cross_validation_Keck_FP', 'cross_validation_RMI']
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', dest="model_name", action="store", required=True)
+    parser.add_argument('--task', dest="task", action="store", required=True)
+    parser.add_argument('--fold_idx', dest="fold_idx", action="store", type=int, required=True)
+    given_args = parser.parse_args()
+
+    model_name = given_args.model_name
+    task = given_args.task
+    fold_idx = given_args.fold_idx
+
+    data = np.load('{}/fold_{}.npz'.format(model_name, fold_idx))
+
+    task_index = task_list.index(task)
+    print task, task_index
+
+    y_train = reshape_data_into_2_dim(data['y_train'][:, task_index])
+    y_val = reshape_data_into_2_dim(data['y_val'][:, task_index])
+    y_test = reshape_data_into_2_dim(data['y_test'][:, task_index])
+    y_pred_on_train = reshape_data_into_2_dim(data['y_pred_on_train'][:, task_index])
+    y_pred_on_val = reshape_data_into_2_dim(data['y_pred_on_val'][:, task_index])
+    y_pred_on_test = reshape_data_into_2_dim(data['y_pred_on_test'][:, task_index])
+
+    print y_train.shape, '\t', y_pred_on_train.shape, '\t', y_test.shape, '\t', y_pred_on_test.shape
+
+    print('train precision: {}'.format(precision_auc_single(y_train, y_pred_on_train)))
+    print('train roc: {}'.format(roc_auc_single(y_train, y_pred_on_train)))
+    print('train bedroc: {}'.format(bedroc_auc_single(y_train, y_pred_on_train)))
+    print
+    print('validation precision: {}'.format(precision_auc_single(y_val, y_pred_on_val)))
+    print('validation roc: {}'.format(roc_auc_single(y_val, y_pred_on_val)))
+    print('validation bedroc: {}'.format(bedroc_auc_single(y_val, y_pred_on_val)))
+    print
+
+    print('test precision: {}'.format(precision_auc_single(y_test, y_pred_on_test)))
+    print('test roc: {}'.format(roc_auc_single(y_test, y_pred_on_test)))
+    print('test bedroc: {}'.format(bedroc_auc_single(y_test, y_pred_on_test)))
+    print
+
+
+    for EF_ratio in [0.02, 0.01, 0.0015, 0.001]:
+        n_actives, ef, ef_max = enrichment_factor_single(y_test, y_pred_on_test, EF_ratio)
+        nef = ef / ef_max
+        print('ratio: {}, EF: {},\tactive: {}'.format(EF_ratio, ef, n_actives))
+        print('ratio: {}, NEF: {}'.format(EF_ratio, nef))
--- output/stage_1_predictions/regenerate_results.sh
+++ output/stage_1_predictions/regenerate_results.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-model_name_list=(single_classification_22 single_classification_42 single_regression_2 single_regression_11 multi_classification_15 multi_classification_18)
+model_name_list=(single_classification_22 single_classification_42 single_regression_2 single_regression_11 multi_classification_15 multi_classification_18 vanilla_lstm_8 vanilla_lstm_19)
 task_list=(cross_validation_Keck_Pria_AS_Retest cross_validation_Keck_FP cross_validation_RMI)
 
 multi_task="${array[@]}"
 single_task=${array[$process]}
-
-model_name_list=(single_classification_22)
-task_list=(cross_validation_Keck_Pria_AS_Retest)
 
 for model_name in "${model_name_list[@]}"
 do
@@ -19,8 +16,7 @@ do
 python regenerate_results.py \
 --task="$task" \
 --model_name="$model_name" \
---fold_idx="$fold_idx"
-#> "$task"/"$model_name"/"$fold_idx".out
+--fold_idx="$fold_idx" > ../stage_1/"$task"/"$model_name"/"$fold_idx".out
 
 done
 done
