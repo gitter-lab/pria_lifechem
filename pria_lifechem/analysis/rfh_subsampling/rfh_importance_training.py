1d2fafc465c0b6b484106cabb6ba3486341cd9e8
Malnammi <alnammi2009@gmail.com>
2018-09-28 14:36:42 -0500

    Update to file naming in RF_h importance training

--- pria_lifechem/analysis/rfh_subsampling/rfh_importance_training.py
+++ pria_lifechem/analysis/rfh_subsampling/rfh_importance_training.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     task.save_model_params(config_csv_file)
                                        
     # save evaluation results
-    output_dir = model_dir+'/rule_{}/'.format(process_num)
+    output_dir = model_dir+'/rule_{}/fold_0/'.format(process_num)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     if not os.path.exists(output_dir+'/train_metrics/'):
